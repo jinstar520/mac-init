7530ab32834de8aaf02e9356a29c6201d7ef1279
AN Meng <littleliar.im@gmail.com>
2017-11-03 01:33:02 -0500

    Update install.sh
    添加 homebrew 国内源

--- install.sh
+++ install.sh
@@ -6,6 +6,14 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# replace homebrew-core
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin git://mirrors.ustc.edu.cn/homebrew-core.git
+
+# replace Homebrew Bottles
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.zshrc
+source ~/.zshrc
+
 # install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 
