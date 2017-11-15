7cd5fad6db572e3f10f991da4ba3c5d3d83facdd
AN Meng <littleliar.im@gmail.com>
2017-11-15 10:42:35 +0800

    fix: 修改 nvm 安装方式
    使用 brew 安装 nvm 会有 nvm prefix 警告，所以修改为脚本安装。

--- install.sh
+++ install.sh
@@ -3,6 +3,18 @@
 # 安装 oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
+# 安装 nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+
+# 设置 nvm 目录及相关脚本
+echo '' >> ~/.zshrc
+echo '# 设置 nvm 目录及相关脚本' >> ~/.zshrc
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.zshrc
+source ~/.zshrc
+
 # 安装 rvm
 \curl -sSL https://get.rvm.io | bash -s stable
 
@@ -37,26 +49,14 @@ cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
 git remote set-url origin git://mirrors.ustc.edu.cn/homebrew-core.git
 
 # 替换 Homebrew Bottles
+echo '' >> ~/.zshrc
 echo '# 替换 Homebrew Bottles' >> ~/.zshrc
 echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.zshrc
-
 
 # 创建工作目录
 mkdir -p ~/workspace/github ~/workspace/oschina ~/workspace/gitlab
 
-# 创建 nvm 工作目录
-mkdir -p ~/.nvm
-
-# 安装 nvm(通过homebrew来安装, 也可通过下面的脚本进行安装)
-# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
-brew install nvm
-
-# 设置 nvm 目录及相关脚本
-echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.zshrc
-echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.zshrc
-
-echo 'nvm 安装完成，先安装 node，然后在通过 brew 来安装 yarn'
+echo '通过 brew 来安装 yarn'
 
 # 安装 yarn(通过homebrew来安装)
 # brew install yarn
